18fa7dba0ccef5131b6d76933fafe4a627001182
iterepa <wizmond705@gmail.com>
2024-05-30 13:06:32 +0300

    added some code to test what i have

--- src/Block.java
+++ src/Block.java
@@ -15,6 +15,16 @@ public class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.timestamp = new Date().getTime();
+        this.hash = calculateHash(); // we need to calculate hash after all other values are set
     }
 
+    /**
+     * This method calculates fingerprint we use to chain blocks
+     */
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applySha256(
+                previousHash + data + Long.toString(timestamp)
+        );
+        return calculatedhash;
+    }
 }
